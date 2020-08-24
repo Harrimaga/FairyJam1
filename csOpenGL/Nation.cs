fa62d57d6c33f6a91216143bbb14f8d0f2bffd7b
ilan hartskeerl <ilan@hartskeerl.nl>
2020-08-24 12:20:03 +0200

    Feiv

---
+++ csOpenGL/Nation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyJam
+{
+
+    class Nation
+    {
+
+        public double Materials { get; set; }
+        public double Fuel { get; set; }
+        public double Food { get; set; }
+        public double Money { get; set; }
+        public double Happiness { get; set; }
+        public double TechPoints { get; set; }
+        public double Population { get; set; }
+
+        public Nation()
+        {
+            Materials = 5;
+            Fuel = 5;
+            Food = 5;
+            Money = 5;
+            Happiness = 5;
+            TechPoints = 5;
+            Population = 5;
+        }
+
+    }
+
+}
