413099ed4c5b74b10a7a3bf22d9546ef75ee76f6
KingTurtleTheFrog <timvanbrederode@live.nl>
2020-08-24 11:24:32 +0200

    Created a GameState

---
+++ csOpenGL/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyJam
+{
+    public enum GameState
+    {
+        MAINMENU,
+        MINUTE,
+        PAUSED,
+        PLAYING
+    }
+}
