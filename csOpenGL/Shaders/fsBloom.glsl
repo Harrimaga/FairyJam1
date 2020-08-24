7f65ab2b77f54ccbe823a33b857279c583610289
ilan hartskeerl <ilan@hartskeerl.nl>
2020-08-24 16:43:29 +0200

    Fixed bloom a bit

--- csOpenGL/Shaders/fsBloom.glsl
+++ csOpenGL/Shaders/fsBloom.glsl
@@ -9,27 +9,18 @@ void main()
 {
     ivec2 pos = ivec2((UV.x+1)*screenSize.x/2, (UV.y+1)*screenSize.y/2);
 	vec3 colour = vec3(0, 0, 0);
-	float num = 0;
 	int bloomX = screenSize.x/480;
-	int bloomY = screenSize.x/270;
+	int bloomY = screenSize.y/270;
 	vec4 baseColour = imageLoad(prev, pos);
+	int bxb = bloomX*bloomY;
 	for(int i=-bloomX;i<bloomX+1;i++){
 		for(int j=-bloomY;j<bloomY+1;j++){
-			ivec2 posi = pos + ivec2(i, j);
-			if(!(posi.x < 0 || posi.y < 0 || posi.x >= screenSize.x || posi.y >= screenSize.y)) 
-			{
-				vec4 c = imageLoad(prev, pos + ivec2(i, j)) - vec4(1, 1, 1, 0);
-				c = clamp(c, vec4(0, 0, 0, 0), vec4(999, 999, 999, 999));
+			vec4 c = imageLoad(prev, pos + ivec2(i, j)) - vec4(1, 1, 1, 0);
+			if(c.x > 0 || c.y > 0 || c.z > 0) {
+				c = max(c, vec4(0, 0, 0, 0)) * (bxb - abs(i*j))/(bxb);
  				colour += c.xyz*c.w;
 			}
-			num++;
 		}
 	}
-	if(num > 0) 
-	{
-		gl_FragColor = baseColour + vec4(colour/num, 0);
-	}
-	else {
-		gl_FragColor = vec4(0, 0, 0, 0);
-	}
+	gl_FragColor = baseColour + vec4(colour/bxb, 0);
 }
