10a5e8fdf2fb6c7832655653969dfcb2750a9988
ilan hartskeerl <ilan@hartskeerl.nl>
2020-08-25 14:01:58 +0200

    Shader fix

--- csOpenGL/Shaders/fs.glsl
+++ csOpenGL/Shaders/fs.glsl
@@ -38,8 +38,8 @@ void main()
 	if(posi.x > int(s.startx + 0.995*s.w/s.scalex)) {
 		posi.x = int(s.startx + 0.995*s.w/s.scalex);
 	}
-	if(posi.y > int(s.starty + 0.995*s.h/s.scalex)) {
-		posi.y = int(s.starty + 0.995*s.h/s.scalex);
+	if(posi.y > int(s.starty + 0.995*s.h/s.scaley)) {
+		posi.y = int(s.starty + 0.995*s.h/s.scaley);
 	}
     vec4 sd = imageLoad(myImage, posi) * vec4(s.r, s.g, s.b, s.a);
     gl_FragColor = sd;
